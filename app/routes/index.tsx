bacbf17c6603e313db23ee4a2d1334998a623d0a
Rakib544 <md.rakib10122003@gmail.com>
2022-06-08 00:50:24 +0600

    feat(route/index): homepage basic title and subtitle added

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,13 @@
 export default function Index() {
   return (
     <div>
-      <h1 className="my-6 color-sky-600">This is our binary coders projects</h1>
+      <h1 className="mt-12 color-sky-600 text-4xl font-bold text-sky-400 text-center">
+        Welcome to Binary Codes project
+      </h1>
+      <p className="text-center text-xl mt-4 text-slate-600">
+        This is just a project for teaching students the basic of programming
+        language
+      </p>
     </div>
   )
 }
