cae47832c9f380527f64b5be761261790db39cc8
Rakib544 <md.rakib10122003@gmail.com>
2022-06-07 23:16:04 +0600

    fix: eslint setup

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -8,13 +8,13 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  let markup = renderToString(
+  const markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />,
   )
 
   responseHeaders.set('Content-Type', 'text/html')
 
-  return new Response('<!DOCTYPE html>' + markup, {
+  return new Response(`<!DOCTYPE html>${  markup}`, {
     status: responseStatusCode,
     headers: responseHeaders,
   })
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,7 @@
 export default function Index() {
   return (
-    <div style={{fontFamily: 'system-ui, sans-serif', lineHeight: '1.4'}}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1 className="my-6 color-sky-600">This is our binary coders projects</h1>
     </div>
   )
 }
