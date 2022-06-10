e3ce43affe90f9418a042b6d62e1eaea5adaaf0e
tanvir1017 <tanvir.hossaindev1@gmail.com>
2022-06-10 20:44:02 +0600

    feat: 🎸 Nothing change but added heading section
    Added a heading design in homepage
    
    BREAKING CHANGE: 🧨 noting

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,8 @@
+import Heading from '../components/heading/heading'
 export default function Index() {
   return (
-    <div>
-      <h1 className='mt-12 color-sky-600 text-4xl font-bold text-sky-400 text-center'>
-        Welcome to Binary Codes project
-      </h1>
-      <p className='text-center text-xl mt-4 text-slate-600'>
-        This is just a project for teaching students the basic concept of programming language.
-      </p>
-    </div>
+    <>
+      <Heading />
+    </>
   )
 }
