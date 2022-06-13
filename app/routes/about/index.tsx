80d85c1056f1c20f8dcfc9fbfd441da2748009f4
proloypaul <proloyp23@gmail.com>
2022-06-13 20:19:42 +0600

    feat: 🎸 added aboutPage

--- app/routes/about/index.tsx
+++ app/routes/about/index.tsx
@@ -1,7 +1,58 @@
 const index = () => {
   return (
-    <div>
-      <h2 className='text-3xl p-10'>This is about page</h2>
+    <div className='mx-8 my-12'>
+      <div className='text-center text-3xl font-bold'>
+        <h1>Student-First Initiative</h1>
+        <h1> for Lower Rates on</h1>
+        <h1>Student Loans,</h1>
+      </div>
+      <div className='flex justify-center items-center py-8'>
+        <div className='lg:flex '>
+          <img
+            src='https://i.ibb.co/mvg4qXt/csaba-balazs-q9-URsedw330-unsplash.jpg'
+            alt='Empty'
+            className=' lg:p-2  lg:w-80 lg:h-80'
+          />
+          <div className='lg:flex-none'>
+            <img
+              src='https://i.ibb.co/mvg4qXt/csaba-balazs-q9-URsedw330-unsplash.jpg'
+              alt='Empty'
+              className=' sm:w-40 lg:py-2 lg:w-40 lg:h-40'
+            />
+            <img
+              src='https://i.ibb.co/mvg4qXt/csaba-balazs-q9-URsedw330-unsplash.jpg'
+              alt='Empty'
+              className=' sm:w-40 lg:pb-2 lg:w-40 lg:h-40'
+            />
+          </div>
+          <img
+            src='https://i.ibb.co/mvg4qXt/csaba-balazs-q9-URsedw330-unsplash.jpg'
+            alt='Empty'
+            className='lg:p-2 lg:w-80 lg:h-80'
+          />
+        </div>
+      </div>
+      <div className='text-center'>
+        <h1 className='text-3xl font-bold'>Our Story</h1>
+        <div className='py-4'>
+          <p>BinaryCodes founder, Abu Jakaria. He is the first person</p>
+          <p>Who think we can build a programmer Group.</p>
+          <p>This web page develop idea! come from Rakib Hossain.</p>
+          <p> He also developed our binaryCodes website.</p>
+          <p>And don&apos;t forgate about Tanvir Hossain</p>
+          <p>Who give&apos;s us how to improve our binary coders.</p>
+          <p>Now we are able to create binaryCoders with joy.</p>
+        </div>
+      </div>
+      {/* our team  */}
+      <div>
+        <h1 className='text-3xl font-bold'>Our team</h1>
+        <div>
+          <div>
+            <img src='' alt='' />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
