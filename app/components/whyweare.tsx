4f7e8737659ab477d1b0c2e9ce1b1ce2e5786762
Rakib544 <md.rakib10122003@gmail.com>
2022-07-20 21:35:00 +0600

    refactor(about): about page subtitle font size updated

--- app/components/whyweare.tsx
+++ app/components/whyweare.tsx
@@ -2,7 +2,7 @@ const WhyWeAre = () => {
   return (
     <div className='my-24 max-w-4xl m-auto'>
       <h2 className='text-3xl font-bold text-gray-700 text-center my-5'>Why we are here ?</h2>
-      <p className='mx-5 md:mx-0 text-lg'>
+      <p className='mx-5 md:mx-0 text-md text-slate-500'>
         Our first and foremost objective is to introduce students to the fundamentals of
         programming. Despite studying in the computer department, many of us do not really know what
         programming is! The victims of this are those of us who are trying to build a community. We
