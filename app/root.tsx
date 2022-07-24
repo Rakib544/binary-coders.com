f8436eade1fa4f38745efc50c35ad3d91a48cbb5
Rakib544 <md.rakib10122003@gmail.com>
2022-07-24 17:45:26 +0600

    fix(bug in notification): tRy to figure out a new bug

--- app/root.tsx
+++ app/root.tsx
@@ -261,7 +261,7 @@ function PostNotification({
       setShow(false)
       setShow(true)
     })
-
+  console.log({ userId })
   return (
     <>
       {show && userId && notificationMessage?.creator.id !== userId && (
